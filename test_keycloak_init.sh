ab16b224871015a2f064171a68e8a7bd99076ce7
Richard Nemeth <ryshoooo@gmail.com>
2022-05-14 11:32:53 +0200

    fix: fix the keycloak init script

--- test_keycloak_init.sh
+++ test_keycloak_init.sh
@@ -14,7 +14,7 @@ function keycloak_start() {
     echo "Starting keycloak docker container"
     docker run -d --name unittest_keycloak -e KEYCLOAK_ADMIN="${KEYCLOAK_ADMIN}" -e KEYCLOAK_ADMIN_PASSWORD="${KEYCLOAK_ADMIN_PASSWORD}" -p "${KEYCLOAK_PORT}:8080" "${KEYCLOAK_DOCKER_IMAGE}" start-dev
     SECONDS=0
-    until curl localhost:8080; do
+    until curl localhost:$KEYCLOAK_PORT; do
       sleep 5;
       if [ ${SECONDS} -gt 180 ]; then
         echo "Timeout exceeded";
@@ -23,7 +23,7 @@ function keycloak_start() {
     done
 }
 
-# Ensuring that postgres is stopped in case of CTRL-C
+# Ensuring that keycloak is stopped in case of CTRL-C
 trap keycloak_stop err exit
 
 keycloak_stop # In case it did not shut down correctly last time.
