2abfd0b39a8bff930c667050f2c9ff77f2af0fb8
Richard Nemeth <ryshoooo@gmail.com>
2022-05-15 17:43:05 +0200

    docs: setup docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,15 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "m2r2",
+    "autoapi.extension",
 ]
+
+autoapi_type = "python"
+autoapi_dirs = ["../../keycloak"]
+autoapi_root = "reference"
+autoapi_keep_files = False
+autoapi_add_toctree_entry = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -103,7 +111,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 
 html_use_smartypants = False
